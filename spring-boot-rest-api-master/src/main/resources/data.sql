9a39e5caf9a772ff0eaa5a9ad90b872a39e30c7b
rachidel12 <elrachidmorabit@gmail.com>
2024-01-15 04:39:35 +0100

    commit

--- spring-boot-rest-api-master/src/main/resources/data.sql
+++ spring-boot-rest-api-master/src/main/resources/data.sql
@@ -34,7 +34,27 @@ INSERT INTO users (name, email, password ) VALUES   ( 'admin', 'admin@mail.com',
 INSERT INTO games (game_id, title, genre, release_date) VALUES
 (1, 'Marvel''s Spider-Man 2', 'Adventure, Hack and slash/Beat''em up', '2023-10-20'),
 (2, 'The Finals', 'Shooter, Tactical', '2023-10-26'),
-(3, 'Call of Duty: Modern Warfare III', 'Shooter', '2023-10-08');
+(3, 'Call of Duty: Modern Warfare III', 'Shooter', '2023-10-08'),
+(4, 'Outer Wilds: Archaeologist Edition', 'Adventure, Indie, Puzzle', '2021-09-28'),
+(5, 'Prince of Persia: The Lost Crown', 'Adventure, Platform', '2024-01-18'),
+(6, 'Gears 5', 'Adventure, Shooter', '2019-09-09'),
+(7, 'The Last of Us Part II', 'Adventure, Shooter', '2020-06-19'),
+(8, 'The Legend of Zelda: Breath of the Wild 2', 'Adventure, Role-playing (RPG)', '2022-12-31'),
+(9, 'The Elder Scrolls VI', 'Adventure, Role-playing (RPG)', '2023-12-31'),
+(10, 'Cyberpunk 2077', 'Adventure, Role-playing (RPG)', '2020-12-10'),
+(11, 'The Witcher 4', 'Adventure, Role-playing (RPG)', '2023-12-31'),
+(12, 'The Elder Scrolls V: Skyrim', 'Adventure, Role-playing (RPG)', '2011-11-11'),
+(13, 'The Witcher 3: Wild Hunt', 'Adventure, Role-playing (RPG)', '2015-05-19'),
+(14, 'The Witcher 2: Assassins of Kings', 'Adventure, Role-playing (RPG)', '2011-05-17'),
+(15, 'The Witcher', 'Adventure, Role-playing (RPG)', '2007-10-26'),
+(16, 'The Elder Scrolls IV: Oblivion', 'Adventure, Role-playing (RPG)', '2006-03-20'),
+(17, 'The Elder Scrolls III: Morrowind', 'Adventure, Role-playing (RPG)', '2002-05-01'),
+(18, 'The Elder Scrolls II: Daggerfall', 'Adventure, Role-playing (RPG)', '1996-08-31'),
+(19, 'The Elder Scrolls: Arena', 'Adventure, Role-playing (RPG)', '1994-03-25'),
+(20, 'The Witcher: Rise of the White Wolf', 'Adventure, Role-playing (RPG)', '2023-12-31'),
+(21, 'The Witcher: Versus', 'Adventure, Role-playing (RPG)', '2023-12-31'),
+(22, 'The Witcher: Crimson Trail', 'Adventure, Role-playing (RPG)', '2023-12-31'),
+(23, 'The Witcher: Adventure Game', 'Adventure, Role-playing (RPG)', '2023-12-31');
 
 -- Sample data for UserLibrary table
 INSERT INTO UserLibrary (userId, gameId) VALUES
